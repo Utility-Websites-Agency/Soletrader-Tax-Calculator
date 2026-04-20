718baddcb25ca2c339e6f587a3c41c5218eb47c7
DSG <dsg@Mac-mini.local>
2026-04-20 16:10:59 +1000

    feat: add Google Analytics GA4 (G-BFRX6S9RWT)

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,12 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3581257773539253"
           crossOrigin="anonymous"
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BFRX6S9RWT" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-BFRX6S9RWT');`,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
